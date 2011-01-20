69c6dc671408bed255f27ad7adbce6034e20b10d
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-01-20 15:32:45 +0100

    Generate setup.sql from migrations.
    closes #37

--- data/db/setup.sql
+++ data/db/setup.sql
@@ -1,129 +1,206 @@
--- phpMyAdmin SQL Dump
--- version 3.1.2
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.1.52, for redhat-linux-gnu (x86_64)
 --
--- Host: localhost
--- Generation Time: Jun 11, 2009 at 11:59 AM
--- Server version: 5.0.76
--- PHP Version: 5.2.9-pl2-gentoo
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
+-- Host: localhost    Database: technogate_zf_tmpl
+-- ------------------------------------------------------
+-- Server version	5.1.52
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: ``
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `debugs`
 --
 
+DROP TABLE IF EXISTS `debugs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `debugs` (
-  `id` bigint(20) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) NOT NULL,
-  `desc` varchar(2048) default NULL,
-  `created_at` DATETIME DEFAULT NULL,
-  `updated_at` DATETIME DEFAULT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=INNODB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `desc` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `debugs`
+--
+
+LOCK TABLES `debugs` WRITE;
+/*!40000 ALTER TABLE `debugs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `debugs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `events`
 --
 
+DROP TABLE IF EXISTS `events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `events` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
-  `thing_id` BIGINT(20) default 0,
-  `thing_type` varchar(45) default NULL,
-  `name` varchar(45) default NULL,
-  `type` varchar(45) default NULL,
-  `parent_id` bigint(20) unsigned default NULL,
-  `parameters` varchar(512) default NULL,
-  `created_at` DATETIME DEFAULT NULL,
-  `updated_at` DATETIME DEFAULT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=INNODB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `thing_id` int(11) NOT NULL,
+  `thing_type` varchar(16) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `parameters` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `events`
+--
+
+LOCK TABLES `events` WRITE;
+/*!40000 ALTER TABLE `events` DISABLE KEYS */;
+/*!40000 ALTER TABLE `events` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `logs`
 --
 
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `logs` (
-  `id` bigint(20) NOT NULL auto_increment,
-  `user_id` bigint(20) default NULL,
-  `session_cookie` varchar(255) default NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `session_cookie` varchar(255) DEFAULT NULL,
   `level` varchar(128) NOT NULL,
-  `ip` varchar(20) default NULL,
+  `ip` varchar(20) DEFAULT NULL,
   `name` varchar(128) NOT NULL,
-  `p1` varchar(255) default NULL,
-  `p2` varchar(255) default NULL,
-  `p3` varchar(255) default NULL,
-  `p4` varchar(255) default NULL,
-  `created_at` DATETIME DEFAULT NULL,
-  `updated_at` DATETIME DEFAULT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=INNODB  DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+  `p1` varchar(255) DEFAULT NULL,
+  `p2` varchar(255) DEFAULT NULL,
+  `p3` varchar(255) DEFAULT NULL,
+  `p4` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `users`
+-- Dumping data for table `logs`
 --
 
-CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL auto_increment,
-  `login` varchar(30) NOT NULL UNIQUE,
-  `hashed_password` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL UNIQUE,
-  `phone` varchar(45) default NULL,
-  `profile_picture` varchar(255) default NULL,
-  `status` varchar(45) default NULL,
-  `blocked` INTEGER(1) NOT NULL DEFAULT 0,
-  `last_login_at` DATETIME DEFAULT NULL,
-  `created_at` DATETIME DEFAULT NULL,
-  `updated_at` DATETIME DEFAULT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=INNODB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+LOCK TABLES `logs` WRITE;
+/*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logs` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `schema_migrations`
+--
+
+DROP TABLE IF EXISTS `schema_migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schema_migrations`
+--
+
+LOCK TABLES `schema_migrations` WRITE;
+/*!40000 ALTER TABLE `schema_migrations` DISABLE KEYS */;
+INSERT INTO `schema_migrations` VALUES ('20110120120910'),('20110120135928'),('20110120140144'),('20110120142340'),('20110120142651');
+/*!40000 ALTER TABLE `schema_migrations` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tokens`
 --
 
+DROP TABLE IF EXISTS `tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tokens` (
-  `id` bigint(20) NOT NULL auto_increment,
-  `hash` varchar(40) NOT NULL UNIQUE,
-  `thing_id` BIGINT(20) default 0,
-  `thing_type` varchar(45) default NULL,
-  `action` varchar(255) NOT NULL,
-  `used` bigint(20) NOT NULL default 0,
-  `auto_delete` INTEGER(1) DEFAULT 1,
-  `status` tinyint(1) NOT NULL,
-  `created_at` DATETIME DEFAULT NULL,
-  `updated_at` DATETIME DEFAULT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=INNODB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hash` varchar(40) NOT NULL,
+  `thing_id` int(11) NOT NULL,
+  `thing_type` varchar(16) NOT NULL,
+  `action` varchar(255) DEFAULT NULL,
+  `used` int(11) DEFAULT NULL,
+  `auto_delete` tinyint(1) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_tokens_on_hash` (`hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- CONSTRAINTS
+-- Dumping data for table `tokens`
 --
 
--- logs
-ALTER TABLE `logs` ADD CONSTRAINT logs_user_id_users_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+LOCK TABLES `tokens` WRITE;
+/*!40000 ALTER TABLE `tokens` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tokens` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(30) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `hashed_password` varchar(255) NOT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `blocked` tinyint(1) NOT NULL DEFAULT '0',
+  `last_login_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_users_on_email` (`email`),
+  UNIQUE KEY `index_users_on_login` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-01-20 15:31:48
